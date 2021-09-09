ab03bb50ff182d33e17d3412f64490c1e943e6a5
Bdey <biplopdey4@gmail.com>
2021-09-09 11:42:04 +0200

    Create getTitle function

--- entryPage/views.py
+++ entryPage/views.py
@@ -1,3 +1,13 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from encyclopedia import util
 
 # Create your views here.
+
+def getTitle(request, title):
+    content = util.get_entry(title)
+    if(content):
+        return HttpResponse(content)
+    else:
+        return HttpResponse("Entry not found")
